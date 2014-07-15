17d946a2881725ab3988efa76581d5c6080237da
Benedikt Ritter <beneritter@gmail.com>
2014-07-15 21:26:40 +0200

    Add script for iterating of jobs and folders

---
+++ src/main/groovy/IterateOverJobs.groovy
@@ -0,0 +1,31 @@
+/**
+ * Iterates over all items, identifing Folders and interating of the jobs inside
+ * of the folders as well. Not that currently folder in folders are not supported
+ */
+import hudson.maven.MavenModuleSet
+import com.cloudbees.hudson.plugins.folder.Folder
+
+/**
+ * All maven jobs ending with "-build"
+ */
+def relevantJobs = { job ->
+  job instanceof MavenModuleSet && job.isBuildable() && job.name.endsWith("-build")
+}
+
+/**
+ * Identify folders
+ */
+def folders = { item ->
+  item instanceof Folder
+}
+
+Jenkins.instance.items.findAll(folders).each{ folder ->
+  folder.items.findAll(relevantJobs).each(handleJob)
+}
+
+
+Jenkins.instance.items.findAll(relevantJobs).each(handleJob)
+
+def handleJob = { job ->
+  // do something with the job...
+}
